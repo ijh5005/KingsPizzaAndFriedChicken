1ba45119a302447d79ebfca11c2babfc3807f0dd
ijh5005 <ijh5005@outlook.com>
2017-02-18 21:45:39 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -1,5 +1,11 @@
 $(document).ready( function () {
 
+	var Track = function () {};
+	var Page = new Track();
+	Page.current = "menu";
+
+	Track.prototype.page = "menu"
+;
 	//slide main menu when option is clicked
 	$(".option").click( function () {
 		var id = $(this).attr("data");
@@ -14,19 +20,40 @@ $(document).ready( function () {
 				//back button fideIn
 				//fade in appropriate page
 				if ( id === "delivery" ){
+					$("#menuPage").addClass("active");
 					$("#menuPage, #back").fadeIn();
+					$(".dish").removeClass("menuImg").addClass("deliverImg");
+					Page.current = "menuPage";
 				}
 				else if ( id === "pickup" ) {
+					$("#menuPage").addClass("active");
 					$("#menuPage, #back").fadeIn();
+					$(".dish").removeClass("menuImg").addClass("pickupImg");
+					Page.current = "menuPage";
 				}
 				else if ( id === "location" ) {
+					$("#locationPage").addClass("active");
 					$("#locationPage, #back").fadeIn();
+					$(".dish").removeClass("menuImg").addClass("locationImg");
+					Page.current = "locationPage";
 				}
 				else if ( id === "review" ) {
+					$("#reviewPage").addClass("active");
 					$("#reviewPage, #back").fadeIn();
+					$(".dish").removeClass("menuImg").addClass("reviewImg");
+					Page.current = "reviewPage";
 				}
 			}
 		});
+	});
+
+	//back button functionality
+	$("#back").click( function () {
+		if (Page.current === "menuPage" | Page.current === "locationPage" | Page.current === "reviewPage") {
+			$(".active").css("display", "none");
+			$("#mainMenu").css("right", 0).fadeIn("slow");
+			$(".dish").attr("class", "").addClass("dish").addClass("menuImg");
+		}
 	});
 
 });
